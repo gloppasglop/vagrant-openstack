208dc1116be189db577fa552570798fbfb94e7a9
Christophe Roux <gloppasglop@gmail.com>
2014-05-24 20:33:56 +0200

    Use Havana release

---
+++ openstack.sh
@@ -0,0 +1,21 @@
+#yum install -y http://rdo.fedorapeople.org/rdo-release.rpm
+yum install -y http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-8.noarch.rpm
+yum install -y openstack-packstack
+if [ ! -d /root/.ssh ]
+then
+  mkdir /root/.ssh
+fi
+chmod 700 /root/.ssh
+cp /vagrant/openstack_rsa /root/.ssh/id_rsa
+cp /vagrant/openstack_rsa.pub /root/.ssh/id_rsa.pub
+cp /vagrant/openstack_rsa /home/vagrant/.ssh/id_rsa
+cp /vagrant/openstack_rsa.pub /root/.ssh/id_rsa.pub
+touch /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+restorecon -R -v /root/.ssh
+grep openstack /root/.ssh/authorized_keys
+if [ $? -ne 0 ] 
+then
+  cat /vagrant/openstack_rsa.pub >> /root/.ssh/authorized_keys
+fi
+packstack --allinone
