dd2837a3931237c13d88aa2bd1f6b738b34d049d
Christophe Roux <gloppasglop@gmail.com>
2014-05-26 13:42:07 +0200

    Ignore image directory

--- openstack.sh
+++ openstack.sh
@@ -1,6 +1,8 @@
 #yum install -y http://rdo.fedorapeople.org/rdo-release.rpm
 yum install -y http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-8.noarch.rpm
 yum install -y openstack-packstack
+yum install -y xauth
+yum install -y virt-manager
 if [ ! -d /root/.ssh ]
 then
   mkdir /root/.ssh
@@ -12,6 +14,7 @@ cp /vagrant/openstack_rsa /home/vagrant/.ssh/id_rsa
 cp /vagrant/openstack_rsa.pub /root/.ssh/id_rsa.pub
 touch /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
+chmod 600  /root/.ssh/id_rsa
 restorecon -R -v /root/.ssh
 grep openstack /root/.ssh/authorized_keys
 if [ $? -ne 0 ] 
